1e0efc50f094c5f21610aa0a97b15ed8e0c6e879
Sherry <Sherry_Zhao@epam.com>
2020-08-31 11:24:15 +0800

    build nuxt project

---
+++ pages/tags.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Please Select One Tag</h1>
+    <ul>
+      <li v-for="tag in tags" :key="tag.name">{{ tag.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tags: [
+        { name: 'Personal', color: 'black' },
+        { name: 'Official', color: 'blue' }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+h1 {
+  color: rgb(134, 135, 209);
+}
+</style>
